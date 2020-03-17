4780803a694f8cb1d560defef4a6f39d3d882c79
g-traub <guillaume.trb@hotmail.com>
2020-03-17 19:03:13 +0100

    06 : pug template (index.pug) to render HTML page for base url ("/")

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Gooood morning vietnam')
+  res.render('index')
 })
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`))
