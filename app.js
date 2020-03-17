672df08664488f4e8121f1497e45216b84a5df50
g-traub <guillaume.trb@hotmail.com>
2020-03-17 19:55:51 +0100

    12 : post created event to the api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config()
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
@@ -12,7 +13,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/party', (req, res) => {
-  res.send('Post ok !')
-  console.log(req.body)
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({ data }) => console.log(data))
+    .catch(err => console.error(err))
 })
 app.listen(port, () => console.log(`Front app listening on port ${port}!`))
