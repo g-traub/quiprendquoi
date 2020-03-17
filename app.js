452f3e4860f2426db9892fcf38ace4a420c2787d
g-traub <guillaume.trb@hotmail.com>
2020-03-17 18:03:38 +0100

    05 : set up env variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Gooood morning vietnam')
