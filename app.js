6de794307509c8b5044b4022a3b9ef458ea54f0c
g-traub <guillaume.trb@hotmail.com>
2020-03-17 19:50:55 +0100

    11 - added body-parser to parse event creation request's data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv').config()
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
@@ -11,5 +13,6 @@ app.get('/', (req, res) => {
 
 app.post('/party', (req, res) => {
   res.send('Post ok !')
+  console.log(req.body)
 })
 app.listen(port, () => console.log(`Front app listening on port ${port}!`))
