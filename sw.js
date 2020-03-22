4e94eee335089f2dc5c88e4fe0493872f061d049
g-traub <guillaume.trb@hotmail.com>
2020-03-22 18:42:09 +0100

    33 : request interception

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
 addEventListener('install', () => {
   console.log('Hello from the service worker')
 })
+
+addEventListener('fetch', event => {
+  if (event.request.headers.get('Accept').includes('text/html')) {
+    event.respondWith(fetch(event.request))
+  }
+})
