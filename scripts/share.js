a114946f38f21214f793ea1b10636f3590697673
g-traub <guillaume.trb@hotmail.com>
2020-03-22 11:45:50 +0100

    28 : added share button for event url with share API

---
+++ scripts/share.js
@@ -0,0 +1,20 @@
+if (navigator.share) {
+  document.querySelectorAll('[data-share-url]').forEach($shareEl => {
+    const $button = document.createElement('button')
+    $button.innerHTML = 'Partager'
+    $shareEl.parentNode.append($button)
+    $button.addEventListener('click', () => share($shareEl, $button))
+  })
+} else {
+  console.warn('Pas de support pour share API')
+}
+
+function share($shareEl) {
+  navigator
+    .share({
+      title: $shareEl.getAttribute('data-share-title'),
+      text: $shareEl.getAttribute('data-share-text'),
+      url: $shareEl.getAttribute('data-share-url')
+    })
+    .catch(err => console.warn(err))
+}
